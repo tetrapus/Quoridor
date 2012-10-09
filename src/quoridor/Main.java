4c49669587b4d0733a59408d3c723f7f401937bd
The1stPlague <lukepearson2@gmail.com>
2012-10-09 14:55:33 +1100

    Added main class and set up basic functions

---
+++ src/quoridor/Main.java
@@ -0,0 +1,20 @@
+package quoridor;
+
+/**
+ * @author Luke
+ * @author Joey Tuong
+ */
+public class Main {
+	int size = 10;
+	Board game = new Board(size);
+	/**
+	 * IO handling function
+	 * gets players and starts the game
+	 */
+	public void createGame(){ 
+	}
+	
+	public void printBoard(Board b){
+		
+	}
+}
