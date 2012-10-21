b35b643babbc5996fd6482fdfcef0b8432b0b75a
the1stplague <lukepearson2@gmail.com>
2012-10-22 03:33:39 +1100

    I think this works

--- src/quoridor/Game.java
+++ src/quoridor/Game.java
@@ -3,8 +3,6 @@ package quoridor;
 
 
 import java.util.LinkedList;
-import java.util.regex.Pattern;
-import java.io.*;
 
 /**
  * Run a game of Quoridor.
@@ -110,23 +108,7 @@ public class Game {
     public void play() {
         while (!finished()) {
             String next = players[board.currentPlayer() - 1].getMove(this);
-            if (Pattern.matches("save.*", next)){
-            	String[] args = next.split("\\s");
-            	
-            	try {
-					FileWriter writer = new FileWriter(args[1]);
-					for (Player p: players){
-						gamedata += p.t
-					}
-					writer.write(players.length+","+)
-				} catch (IOException e) {
-					
-					e.printStackTrace();
-				}
-            
-            } else if (Pattern.matches("load\\s.*", next)){
-            	
-            } else if (isValidMove(next)) {
+            if (isValidMove(next)) {
                 if (next.equals("redo")) {
                     redo();
                 } else if (next.equals("undo")) {
